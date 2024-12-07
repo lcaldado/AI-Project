a1a14faefadcaf294562ede0dd4b21f86578dee2
lcaldado <luciacaldadorein@gmail.com>
2024-12-07 17:29:16 -0500

    Add files via upload

--- MFIS_Read_Functions.py
+++ MFIS_Read_Functions.py
@@ -26,7 +26,7 @@ def readFuzzySetsFile(fleName):
         b = int(elementsList[4])
         c = int(elementsList[5])
         d = int(elementsList[6])
-        x = np.arange(xmin-1,xmax+1)
+        x = np.arange(xmin,xmax,1)
         y = skf.trapmf(x, [a, b, c, d])
         fuzzySet.x = x
         fuzzySet.y = y
@@ -37,7 +37,7 @@ def readFuzzySetsFile(fleName):
     return fuzzySetsDict
 
 def readRulesFile():
-    inputFile = open('Files/Rules.txt', 'r')
+    inputFile = open('Rules.txt', 'r')
     rules = RuleList()
     line = inputFile.readline()
     while line != '':
@@ -56,7 +56,7 @@ def readRulesFile():
     return rules
 
 def readApplicationsFile():
-    inputFile = open('Files/Applications.txt', 'r')
+    inputFile = open('Applications.txt', 'r')
     applicationList = []
     line = inputFile.readline()
     while line != '':
